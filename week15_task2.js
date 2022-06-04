4c56133098177f973701b1ba0e24090544e4353c
Alina Shegay <shegay.alina@gmail.com>
2022-06-04 23:29:26 +0600

    updated calculator and form

---
+++ week15_task2.js
@@ -0,0 +1,23 @@
+function check()
+{
+    let email1 = document.getElementById("exampleInputEmail1");
+    let email2 = documment.getElementById("exampleInputEmail2");
+    let password = document.getElementById("exampleInputPassword");
+    let userName = document.getElementById("exampleInputUserName");
+
+    document.getElementById(`errorMessage`).innerHTML= "";
+
+    if(email1.value == '') {
+        document.getElementById('errorMessage').innerHTML += "Ваш email не заполнен<br>";
+    }
+
+    if(password.value == '') {
+        document.getElementById('errorMessage').innerHTML += "Ваш пароль не заполнен<br>";
+    }
+
+    if(password.value.length<=5) {
+        document.getElementById('errorMessage').innerHTML += "Ваш пароль слишком короткий<br>";
+    }
+
+
+}
